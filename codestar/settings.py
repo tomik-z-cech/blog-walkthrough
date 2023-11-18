63feba2857f0f1ede7b4069c5c399b19eb97bb98
tomik-z-cech <tkubancik@gmail.com>
2023-11-18 20:58:55 +0000

    Add commenting functionality

--- codestar/settings.py
+++ codestar/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -60,6 +61,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_SIGNUP_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
